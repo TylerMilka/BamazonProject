8945c1c1239fcd06a108c6ba87ff763970589087
Tyler <tgmilka@gmail.com>
2017-01-13 16:45:34 -0600

    Revising Some things

--- Bamazon/bamazon.js
+++ Bamazon/bamazon.js
@@ -22,22 +22,29 @@ connection.query("SELECT * FROM products", function(err, res) {  //MYSQL workben
   for (var i = 0; i < res.length; i++){
     console.log(res[i].item_id + " | " +  res[i].product_name + " | " + res[i].department_name + " | " + "$" + res[i].cost + " | ", res[i].stock_quantity);
   }
- for (var i = 0; i < res.length; i++){ //puts all item_ids into an array
-    var choiceArray = [];
-    choiceArray.push(res[i].item_id);
-    console.log(choiceArray)
-    }     
+    
   console.log("-----------------------------------");
-    connection.query("SELECT item_id AND product_name FROM products", function(err, res){
-  inquirer. prompt({ //asking the user to include the item number of product they would like to buy
-    name: "item_id",
+  startBuying()
+})  
+
+var startBuying = function(){
+  inquirer.prompt([
+  {
     type: "input",
-    message: "What item were you interested in buying? [Insert Item Number]",
-    }).then(function(answer){   
-    console.log(answer);
-    console.log(res);
-    })
-  })
-})
+    message: "\n Product ID?",
+    name: "item_id"
+  },
+  
+]).then(function (answer) {
+  console.log(answer.item_id);
+  id = answer.item_id;
+  if(id){
+    productByID();
+    next = true;
+  }
+  if(next === true){
+  howManyUnits(); 
+  }
+});
 
 
